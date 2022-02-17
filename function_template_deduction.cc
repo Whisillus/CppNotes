8374a247d30d436ba5ed435bf12988c063434bc6
nielass <jdchen_97@outlook.com>
2022-02-17 10:45:03 +0100

    deduction define

---
+++ function_template_deduction.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+T max(T v1, T v2) {
+    return v1 < v2 ? v2 : v1;
+}
+
+// Default arguments for deduction
+template <typename T>
+void defaultFuntionValue(T = "");
+// It is okay:
+// defaultFunctionValue(1);
+// we cannot use like this for type deduction reason:
+// defaultFuncitonValue();
+// but we can do following
+template <typename T = std::string>
+void defaultTemplateValue(T = "");
+// It is okay:
+// defaultTemplateValue();
+//
+
+// paras. type deduction
+// rule 1: When declaring call parameters by reference, even trivial conversions
+// do not apply to type deduction.
+// rule 2: when declaring call parameters by value, only trivial conversions
+// that decay are supported:
+// const/volatile are ignored
+// references => referenced type
+// raw array/functions => pointer
+
+int main() {
+    int i = 123;
+    int const ci = 1234;
+    int& ri = i;
+    int arr[3];
+
+    ::max(i, ci);    // T is int
+    ::max(ci, ci);   // T is int
+    ::max(i, ri);    // T is int
+    ::max(&i, arr);  // T is int*
+
+    //::max(4, 1.2); error
+    // std::string s;
+    //::max("123", s); error
+
+    return 0;
+}
